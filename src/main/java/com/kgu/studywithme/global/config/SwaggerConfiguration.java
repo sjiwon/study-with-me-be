eb3dfbafb6d489d61cec9788e3b644fd4beb5803
SeoJiWon <sjiwon4491@gmail.com>
2023-07-08 00:37:45 +0900

    feat: Swagger 설정 추가

---
+++ src/main/java/com/kgu/studywithme/global/config/SwaggerConfiguration.java
@@ -0,0 +1,68 @@
+package com.kgu.studywithme.global.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.customizers.OpenApiCustomizer;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Study With Me API 명세서",
+                description = "Study With Me API 명세서",
+                contact = @Contact(
+                        name = "서지원",
+                        url = "https://github.com/sjiwon/study-with-me-be",
+                        email = "sjiwo4491@gmail.com"
+                ),
+                version = "v1"
+        ),
+        servers = {
+                @Server(
+                        url = "http://localhost:8080",
+                        description = "Local Server"
+                ),
+                @Server(
+                        url = "https://api.study-with-me.co.kr",
+                        description = "Production Server [Currently Disabled]"
+                )
+        }
+)
+public class SwaggerConfiguration {
+    @Bean
+    public GroupedOpenApi studyWithMeApi() {
+        return GroupedOpenApi.builder()
+                .group("Study With Me API 명세서")
+                .pathsToMatch(
+                        "/api/**"
+                )
+                .addOpenApiCustomizer(buildSecurityOpenApi())
+                .build();
+    }
+
+    public OpenApiCustomizer buildSecurityOpenApi() {
+        return openApi -> openApi
+                .addSecurityItem(
+                        new SecurityRequirement()
+                                .addList("JWT Token")
+                )
+                .getComponents()
+                .addSecuritySchemes(
+                        "JWT Token",
+                        new SecurityScheme()
+                                .name(AUTHORIZATION)
+                                .type(SecurityScheme.Type.HTTP)
+                                .in(SecurityScheme.In.HEADER)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                );
+    }
+}
