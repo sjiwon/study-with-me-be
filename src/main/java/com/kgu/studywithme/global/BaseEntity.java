0d8dca788bc985b074db2b5abbccc66138b85562
SeoJiWon <sjiwon4491@gmail.com>
2023-07-13 14:26:26 +0900

    refactor: BaseEntity Auditing 의존 제거 및 테스트용 apply 메소드 추가

--- src/main/java/com/kgu/studywithme/global/BaseEntity.java
+++ src/main/java/com/kgu/studywithme/global/BaseEntity.java
@@ -1,24 +1,41 @@
 package com.kgu.studywithme.global;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.MappedSuperclass;
+import com.google.common.annotations.VisibleForTesting;
+import jakarta.persistence.*;
 import lombok.Getter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
-public abstract class BaseEntity {
-    @CreatedDate
+public abstract class BaseEntity<T> {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
 
-    @LastModifiedDate
-    @Column(name = "modified_at")
-    private LocalDateTime modifiedAt;
+    @Column(name = "last_modified_at")
+    private LocalDateTime lastModifiedAt;
+
+    @PrePersist
+    void prePersist() {
+        LocalDateTime now = LocalDateTime.now();
+        createdAt = now;
+        lastModifiedAt = now;
+    }
+
+    @PreUpdate
+    void preUpdate() {
+        lastModifiedAt = LocalDateTime.now();
+    }
+
+    @VisibleForTesting
+    public T apply(final long id, final LocalDateTime now) {
+        this.id = id;
+        this.createdAt = now;
+        this.lastModifiedAt = now;
+        return (T) this;
+    }
 }
