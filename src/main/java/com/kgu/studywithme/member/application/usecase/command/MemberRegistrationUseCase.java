33533232684c13e4789c0c3d48d600562eb1e418
SeoJiWon <sjiwon4491@gmail.com>
2023-07-14 20:39:09 +0900

    feat: 사용자 회원가입을 위한 `MemberRegistrationUseCase`

---
+++ src/main/java/com/kgu/studywithme/member/application/usecase/command/MemberRegistrationUseCase.java
@@ -0,0 +1,37 @@
+package com.kgu.studywithme.member.application.usecase.command;
+
+import com.kgu.studywithme.category.domain.Category;
+import com.kgu.studywithme.member.domain.*;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+public interface MemberRegistrationUseCase {
+    Long registration(Command command);
+
+    record Command(
+            String name,
+            Nickname nickname,
+            Email email,
+            LocalDate birth,
+            String phone,
+            Gender gender,
+            Region region,
+            boolean emailOptIn,
+            Set<Category> categories
+    ) {
+        public Member toEntity() {
+            return Member.createMember(
+                    name,
+                    nickname,
+                    email,
+                    birth,
+                    phone,
+                    gender,
+                    region,
+                    emailOptIn,
+                    categories
+            );
+        }
+    }
+}
