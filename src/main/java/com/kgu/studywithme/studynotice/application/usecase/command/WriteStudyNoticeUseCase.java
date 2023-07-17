daa0e91344ba0aaecb8cd87c737f46543e02e60b
SeoJiWon <sjiwon4491@gmail.com>
2023-07-17 21:23:53 +0900

    feat: 스터디 공지사항 작성 UseCase 구현

--- src/main/java/com/kgu/studywithme/studynotice/application/usecase/command/WriteStudyNoticeUseCase.java
+++ src/main/java/com/kgu/studywithme/studynotice/application/usecase/command/WriteStudyNoticeUseCase.java
@@ -4,6 +4,7 @@ public interface WriteStudyNoticeUseCase {
     Long writeNotice(final Command command);
 
     record Command(
+            Long hostId,
             Long studyId,
             String title,
             String content
