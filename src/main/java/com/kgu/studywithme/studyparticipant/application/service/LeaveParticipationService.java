c1a8565fe2bcb3c4a156682f83cddea371962716
SeoJiWon <sjiwon4491@gmail.com>
2023-07-19 20:53:28 +0900

    feat: 스터디 참여 취소 UseCase 구현

---
+++ src/main/java/com/kgu/studywithme/studyparticipant/application/service/LeaveParticipationService.java
@@ -0,0 +1,35 @@
+package com.kgu.studywithme.studyparticipant.application.service;
+
+import com.kgu.studywithme.global.annotation.StudyWithMeWritableTransactional;
+import com.kgu.studywithme.global.exception.StudyWithMeException;
+import com.kgu.studywithme.study.application.QueryStudyByIdService;
+import com.kgu.studywithme.study.domain.Study;
+import com.kgu.studywithme.studyparticipant.application.usecase.command.LeaveParticipationUseCase;
+import com.kgu.studywithme.studyparticipant.domain.StudyParticipantRepository;
+import com.kgu.studywithme.studyparticipant.exception.StudyParticipantErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import static com.kgu.studywithme.studyparticipant.domain.ParticipantStatus.LEAVE;
+
+@Service
+@StudyWithMeWritableTransactional
+@RequiredArgsConstructor
+public class LeaveParticipationService implements LeaveParticipationUseCase {
+    private final QueryStudyByIdService queryStudyByIdService;
+    private final StudyParticipantRepository studyParticipantRepository;
+
+    @Override
+    public void leaveParticipation(final Command command) {
+        final Study study = queryStudyByIdService.findById(command.studyId());
+        validateMemberIsHost(study, command.participantId());
+
+        studyParticipantRepository.updateParticipantStatus(command.studyId(), command.participantId(), LEAVE);
+    }
+
+    private void validateMemberIsHost(final Study study, final Long participantId) {
+        if (study.isHost(participantId)) {
+            throw StudyWithMeException.type(StudyParticipantErrorCode.HOST_CANNOT_LEAVE_STUDY);
+        }
+    }
+}
---
+++ src/test/java/com/kgu/studywithme/studyparticipant/application/service/LeaveParticipationServiceTest.java
@@ -0,0 +1,88 @@
+package com.kgu.studywithme.studyparticipant.application.service;
+
+import com.kgu.studywithme.common.UseCaseTest;
+import com.kgu.studywithme.global.exception.StudyWithMeException;
+import com.kgu.studywithme.member.domain.Member;
+import com.kgu.studywithme.study.application.QueryStudyByIdService;
+import com.kgu.studywithme.study.domain.Study;
+import com.kgu.studywithme.studyparticipant.application.usecase.command.LeaveParticipationUseCase;
+import com.kgu.studywithme.studyparticipant.domain.StudyParticipantRepository;
+import com.kgu.studywithme.studyparticipant.exception.StudyParticipantErrorCode;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import java.time.LocalDateTime;
+
+import static com.kgu.studywithme.fixture.MemberFixture.GHOST;
+import static com.kgu.studywithme.fixture.MemberFixture.JIWON;
+import static com.kgu.studywithme.fixture.StudyFixture.SPRING;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@DisplayName("StudyParticipant -> LeaveParticipationService 테스트")
+class LeaveParticipationServiceTest extends UseCaseTest {
+    @InjectMocks
+    private LeaveParticipationService leaveParticipationService;
+
+    @Mock
+    private QueryStudyByIdService queryStudyByIdService;
+
+    @Mock
+    private StudyParticipantRepository studyParticipantRepository;
+
+    private final Member host = JIWON.toMember().apply(1L, LocalDateTime.now());
+    private final Member participant = GHOST.toMember().apply(2L, LocalDateTime.now());
+    private Study study;
+
+    @BeforeEach
+    void setUp() {
+        study = SPRING.toOnlineStudy(host.getId()).apply(1L, LocalDateTime.now());
+    }
+
+    @Test
+    @DisplayName("스터디 팀장은 팀장 권한을 위임하지 않으면 스터디 참여를 취소할 수 없다")
+    void throwExceptionByHostCannotLeaveStudy() {
+        // given
+        given(queryStudyByIdService.findById(any())).willReturn(study);
+
+        // when - then
+        assertThatThrownBy(() -> leaveParticipationService.leaveParticipation(
+                new LeaveParticipationUseCase.Command(
+                        study.getId(),
+                        host.getId()
+                )
+        ))
+                .isInstanceOf(StudyWithMeException.class)
+                .hasMessage(StudyParticipantErrorCode.HOST_CANNOT_LEAVE_STUDY.getMessage());
+
+        verify(queryStudyByIdService, times(1)).findById(any());
+        verify(studyParticipantRepository, times(0))
+                .updateParticipantStatus(any(), any(), any());
+    }
+
+    @Test
+    @DisplayName("스터디 참여를 취소한다")
+    void success() {
+        // given
+        given(queryStudyByIdService.findById(any())).willReturn(study);
+
+        // when
+        leaveParticipationService.leaveParticipation(
+                new LeaveParticipationUseCase.Command(
+                        study.getId(),
+                        participant.getId()
+                )
+        );
+
+        // then
+        verify(queryStudyByIdService, times(1)).findById(any());
+        verify(studyParticipantRepository, times(1))
+                .updateParticipantStatus(any(), any(), any());
+    }
+}
