04fae5d7ea9939e4882b5fed8a75f789c46d7633
SeoJiWon <sjiwon4491@gmail.com>
2023-07-22 11:13:10 +0900

    rename: StudyParticipationEventListener -> StudyAssociatedEmailSendEventListener

--- src/main/java/com/kgu/studywithme/global/infrastructure/mail/event/StudyAssociatedEmailSendEventListener.java
+++ src/main/java/com/kgu/studywithme/global/infrastructure/mail/event/StudyAssociatedEmailSendEventListener.java
@@ -13,7 +13,7 @@ import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 @RequiredArgsConstructor
-public class StudyParticipationEventListener {
+public class StudyAssociatedEmailSendEventListener {
     private final EmailSender emailSender;
 
     @Async("emailAsyncExecutor")
