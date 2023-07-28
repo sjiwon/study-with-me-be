66b2e59871c0fdd0c317901f8a862ce9284c3a36
SeoJiWon <sjiwon4491@gmail.com>
2023-07-28 20:35:24 +0900

    refactor: MySQL TestContainers 설정 애노테이션 분리

---
+++ src/test/java/com/kgu/studywithme/common/MySqlTestContainers.java
@@ -0,0 +1,15 @@
+package com.kgu.studywithme.common;
+
+import com.kgu.studywithme.common.config.MySqlTestContainersConfiguration;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@ContextConfiguration(initializers = MySqlTestContainersConfiguration.Initializer.class)
+public @interface MySqlTestContainers {
+}
--- src/test/java/com/kgu/studywithme/common/RepositoryTest.java
+++ src/test/java/com/kgu/studywithme/common/RepositoryTest.java
@@ -1,16 +1,14 @@
 package com.kgu.studywithme.common;
 
-import com.kgu.studywithme.common.config.MySqlTestContainersConfiguration;
 import com.kgu.studywithme.global.config.P6SpyConfiguration;
 import com.kgu.studywithme.global.config.QueryDslConfiguration;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ContextConfiguration;
 
 @DataJpaTest(showSql = false)
 @Import({QueryDslConfiguration.class, P6SpyConfiguration.class})
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@ContextConfiguration(initializers = MySqlTestContainersConfiguration.Initializer.class)
+@MySqlTestContainers
 public abstract class RepositoryTest {
 }
