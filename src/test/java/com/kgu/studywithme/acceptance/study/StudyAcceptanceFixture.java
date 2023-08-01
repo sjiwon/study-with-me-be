bd3b9364db2df98f42ce480e44635b0752b6985e
SeoJiWon <sjiwon4491@gmail.com>
2023-08-01 17:06:44 +0900

    feat: 스터디 수정/종료 API 인수 테스트 작성

--- src/test/java/com/kgu/studywithme/acceptance/study/StudyAcceptanceFixture.java
+++ src/test/java/com/kgu/studywithme/acceptance/study/StudyAcceptanceFixture.java
@@ -2,13 +2,17 @@ package com.kgu.studywithme.acceptance.study;
 
 import com.kgu.studywithme.common.fixture.StudyFixture;
 import com.kgu.studywithme.study.presentation.dto.request.CreateStudyRequest;
+import com.kgu.studywithme.study.presentation.dto.request.UpdateStudyRequest;
 import io.restassured.response.ValidatableResponse;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import static com.kgu.studywithme.acceptance.CommonRequestFixture.postRequest;
+import static com.kgu.studywithme.acceptance.CommonRequestFixture.*;
 
 public class StudyAcceptanceFixture {
-    public static ValidatableResponse 스터디를_생성한다(final String accessToken, final StudyFixture fixture) {
+    public static ValidatableResponse 스터디를_생성한다(
+            final String accessToken,
+            final StudyFixture fixture
+    ) {
         final String uri = UriComponentsBuilder
                 .fromPath("/api/study")
                 .build()
@@ -31,10 +35,52 @@ public class StudyAcceptanceFixture {
         return postRequest(accessToken, request, uri);
     }
 
-    public static Long 스터디를_생성하고_PK를_얻는다(final String accessToken, final StudyFixture fixture) {
+    public static Long 스터디를_생성하고_PK를_얻는다(
+            final String accessToken,
+            final StudyFixture fixture
+    ) {
         return 스터디를_생성한다(accessToken, fixture)
                 .extract()
                 .jsonPath()
                 .getLong("studyId");
+    }
+
+    public static ValidatableResponse 스터디를_수정한다(
+            final String accessToken,
+            final Long studyId,
+            final StudyFixture fixture
+    ) {
+        final String uri = UriComponentsBuilder
+                .fromPath("/api/studies/{studyId}")
+                .build(studyId)
+                .getPath();
+
+        final UpdateStudyRequest request = new UpdateStudyRequest(
+                fixture.getName().getValue(),
+                fixture.getDescription().getValue(),
+                fixture.getCapacity().getValue(),
+                fixture.getCategory().getId(),
+                fixture.getThumbnail().getImageName(),
+                fixture.getType().getValue(),
+                (fixture.getLocation() == null) ? null : fixture.getLocation().getProvince(),
+                (fixture.getLocation() == null) ? null : fixture.getLocation().getCity(),
+                true,
+                fixture.getMinimumAttendanceForGraduation(),
+                fixture.getHashtags()
+        );
+
+        return patchRequest(accessToken, request, uri);
+    }
+
+    public static ValidatableResponse 스터디를_종료시킨다(
+            final String accessToken,
+            final Long studyId
+    ) {
+        final String uri = UriComponentsBuilder
+                .fromPath("/api/studies/{studyId}")
+                .build(studyId)
+                .getPath();
+
+        return deleteRequest(accessToken, uri);
     }
 }
--- src/test/java/com/kgu/studywithme/acceptance/study/StudyAcceptanceTest.java
+++ src/test/java/com/kgu/studywithme/acceptance/study/StudyAcceptanceTest.java
@@ -2,28 +2,62 @@ package com.kgu.studywithme.acceptance.study;
 
 import com.kgu.studywithme.common.AcceptanceTest;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import static com.kgu.studywithme.acceptance.study.StudyAcceptanceFixture.스터디를_생성한다;
+import static com.kgu.studywithme.acceptance.study.StudyAcceptanceFixture.*;
 import static com.kgu.studywithme.common.fixture.MemberFixture.JIWON;
+import static com.kgu.studywithme.common.fixture.StudyFixture.JPA;
 import static com.kgu.studywithme.common.fixture.StudyFixture.SPRING;
 import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
 
 @DisplayName("[Acceptance Test] 스터디 관련 기능")
 public class StudyAcceptanceTest extends AcceptanceTest {
+    private String hostAccessToken;
+
+    @BeforeEach
+    void setUp() {
+        hostAccessToken = JIWON.회원가입_후_Google_OAuth_로그인을_진행한다().accessToken();
+    }
+
     @Nested
     @DisplayName("스터디 생성 API")
     class CreateStudyApi {
         @Test
         @DisplayName("스터디를 생성한다")
-        void createStudyApi() {
-            final String hostAccessToken = JIWON.회원가입_후_Google_OAuth_로그인을_진행한다().accessToken();
-
+        void success() {
             스터디를_생성한다(hostAccessToken, SPRING)
                     .statusCode(CREATED.value())
                     .body("studyId", Matchers.notNullValue(Long.class));
+        }
+    }
+
+    @Nested
+    @DisplayName("스터디 수정 API")
+    class UpdateStudyApi {
+        @Test
+        @DisplayName("스터디를 수정한다")
+        void success() {
+            final Long studyId = SPRING.스터디를_생성한다(hostAccessToken);
+
+            스터디를_수정한다(hostAccessToken, studyId, JPA)
+                    .statusCode(NO_CONTENT.value());
+        }
+    }
+
+    @Nested
+    @DisplayName("스터디 종료 API")
+    class TerminateStudyApi {
+        @Test
+        @DisplayName("스터디를 종료시킨다")
+        void success() {
+            final Long studyId = SPRING.스터디를_생성한다(hostAccessToken);
+
+            스터디를_종료시킨다(hostAccessToken, studyId)
+                    .statusCode(NO_CONTENT.value());
         }
     }
 }
