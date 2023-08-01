8301a2edde498b9f9a2c4541f1919064a556dc83
SeoJiWon <sjiwon4491@gmail.com>
2023-08-01 12:45:03 +0900

    refactor: UploadAcceptanceTest AccessToken `@BeforeEach`로 분리

--- src/test/java/com/kgu/studywithme/acceptance/upload/UploadAcceptanceTest.java
+++ src/test/java/com/kgu/studywithme/acceptance/upload/UploadAcceptanceTest.java
@@ -1,6 +1,7 @@
 package com.kgu.studywithme.acceptance.upload;
 
 import com.kgu.studywithme.common.AcceptanceTest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,11 +13,16 @@ import static org.springframework.http.HttpStatus.OK;
 
 @DisplayName("[Acceptance Test] 파일 업로드 관련 기능")
 public class UploadAcceptanceTest extends AcceptanceTest {
+    private String accessToken;
+
+    @BeforeEach
+    void setUp() {
+        accessToken = JIWON.회원가입_후_Google_OAuth_로그인을_진행한다().accessToken();
+    }
+
     @Test
     @DisplayName("스터디 주차 글 내부 이미지를 업로드한다")
     void uploadStudyWeeklyImageApi() {
-        final String accessToken = JIWON.회원가입_후_Google_OAuth_로그인을_진행한다().accessToken();
-
         스터디_주차_글_내부_이미지를_업로드한다(accessToken)
                 .statusCode(OK.value())
                 .body("result", is("https://weekly-image"));
@@ -25,8 +31,6 @@ public class UploadAcceptanceTest extends AcceptanceTest {
     @Test
     @DisplayName("스터디 설명 내부 이미지를 업로드한다")
     void uploadStudyDescriptionImageApi() {
-        final String accessToken = JIWON.회원가입_후_Google_OAuth_로그인을_진행한다().accessToken();
-
         스터디_설명_내부_이미지를_업로드한다(accessToken)
                 .statusCode(OK.value())
                 .body("result", is("https://study-description-image"));
