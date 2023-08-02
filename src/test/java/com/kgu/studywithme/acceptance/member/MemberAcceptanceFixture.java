3ac804b6d9b17073653a9c08cb7a6d01724318c7
SeoJiWon <sjiwon4491@gmail.com>
2023-08-02 19:08:42 +0900

    feat: 사용자 리뷰, 신고 API 인수 테스트 작성

--- src/test/java/com/kgu/studywithme/acceptance/member/MemberAcceptanceFixture.java
+++ src/test/java/com/kgu/studywithme/acceptance/member/MemberAcceptanceFixture.java
@@ -4,6 +4,9 @@ import com.kgu.studywithme.category.domain.Category;
 import com.kgu.studywithme.common.fixture.MemberFixture;
 import com.kgu.studywithme.member.presentation.dto.request.SignUpMemberRequest;
 import com.kgu.studywithme.member.presentation.dto.request.UpdateMemberRequest;
+import com.kgu.studywithme.memberreport.presentation.dto.request.ReportMemberRequest;
+import com.kgu.studywithme.memberreview.presentation.dto.request.UpdateMemberReviewRequest;
+import com.kgu.studywithme.memberreview.presentation.dto.request.WriteMemberReviewRequest;
 import io.restassured.response.ValidatableResponse;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -39,7 +42,10 @@ public class MemberAcceptanceFixture {
         return postRequest(request, uri);
     }
 
-    public static ValidatableResponse 사용자_정보를_수정한다(final String accessToken, final MemberFixture fixture) {
+    public static ValidatableResponse 사용자_정보를_수정한다(
+            final String accessToken,
+            final MemberFixture fixture
+    ) {
         final String uri = UriComponentsBuilder
                 .fromPath("/api/members/me")
                 .build()
@@ -59,5 +65,47 @@ public class MemberAcceptanceFixture {
         );
 
         return patchRequest(accessToken, request, uri);
+    }
+
+    public static ValidatableResponse 해당_사용자에게_리뷰를_작성한다(
+            final String accessToken,
+            final Long revieweeId
+    ) {
+        final String uri = UriComponentsBuilder
+                .fromPath("/api/members/{revieweeId}/review")
+                .build(revieweeId)
+                .getPath();
+
+        final WriteMemberReviewRequest request = new WriteMemberReviewRequest("Good!!");
+
+        return postRequest(accessToken, request, uri);
+    }
+
+    public static ValidatableResponse 작성한_리뷰를_수정한다(
+            final String accessToken,
+            final Long revieweeId
+    ) {
+        final String uri = UriComponentsBuilder
+                .fromPath("/api/members/{revieweeId}/review")
+                .build(revieweeId)
+                .getPath();
+
+        final UpdateMemberReviewRequest request = new UpdateMemberReviewRequest("Bad...");
+
+        return patchRequest(accessToken, request, uri);
+    }
+
+    public static ValidatableResponse 해당_사용자를_신고한다(
+            final String accessToken,
+            final Long reporteeId
+    ) {
+        final String uri = UriComponentsBuilder
+                .fromPath("/api/members/{reporteeId}/report")
+                .build(reporteeId)
+                .getPath();
+
+        final ReportMemberRequest request = new ReportMemberRequest("매번 지각하고 과제도 제출안해요");
+
+        return postRequest(accessToken, request, uri);
     }
 }
--- src/test/java/com/kgu/studywithme/acceptance/member/MemberReviewAcceptanceTest.java
+++ src/test/java/com/kgu/studywithme/acceptance/member/MemberReviewAcceptanceTest.java
@@ -1,8 +1,86 @@
 package com.kgu.studywithme.acceptance.member;
 
 import com.kgu.studywithme.common.AcceptanceTest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static com.kgu.studywithme.acceptance.member.MemberAcceptanceFixture.작성한_리뷰를_수정한다;
+import static com.kgu.studywithme.acceptance.member.MemberAcceptanceFixture.해당_사용자에게_리뷰를_작성한다;
+import static com.kgu.studywithme.acceptance.study.StudyAcceptanceFixture.*;
+import static com.kgu.studywithme.common.fixture.MemberFixture.GHOST;
+import static com.kgu.studywithme.common.fixture.MemberFixture.JIWON;
+import static com.kgu.studywithme.common.fixture.StudyFixture.SPRING;
+import static com.kgu.studywithme.common.fixture.StudyWeeklyFixture.STUDY_WEEKLY_1;
+import static org.springframework.http.HttpStatus.*;
 
 @DisplayName("[Acceptance Test] 사용자 리뷰 관련 기능")
 public class MemberReviewAcceptanceTest extends AcceptanceTest {
+    private Long hostId;
+    private String hostAccessToken;
+    private Long memberId;
+    private String memberAccessToken;
+    private Long studyId;
+
+    @BeforeEach
+    void setUp() {
+        hostId = JIWON.회원가입을_진행한다();
+        hostAccessToken = JIWON.로그인을_진행한다().accessToken();
+        memberId = GHOST.회원가입을_진행한다();
+        memberAccessToken = GHOST.로그인을_진행한다().accessToken();
+
+        studyId = 스터디를_생성하고_PK를_얻는다(hostAccessToken, SPRING);
+        스터디_참여_신청을_한다(memberAccessToken, studyId);
+    }
+
+    @Nested
+    @DisplayName("사용자 리뷰 작성 API")
+    class writeReview {
+        @Test
+        @DisplayName("셀프 리뷰는 허용하지 않는다")
+        void selfReviewNotAllowed() {
+            해당_사용자에게_리뷰를_작성한다(hostAccessToken, hostId)
+                    .statusCode(CONFLICT.value());
+        }
+
+        @Test
+        @DisplayName("같이 스터디를 진행한 적이 없으면 리뷰를 남길 수 없다")
+        void commonStudyRecordNotFound() {
+            해당_사용자에게_리뷰를_작성한다(hostAccessToken, memberId)
+                    .statusCode(CONFLICT.value());
+        }
+
+        @Test
+        @DisplayName("해당 사용자에게 리뷰를 작성한다")
+        void success() {
+            스터디_신청자에_대한_참여를_승인한다(hostAccessToken, studyId, memberId);
+            스터디_주차를_생성한다(hostAccessToken, studyId, STUDY_WEEKLY_1);
+
+            해당_사용자에게_리뷰를_작성한다(hostAccessToken, memberId)
+                    .statusCode(OK.value());
+        }
+    }
+
+    @Nested
+    @DisplayName("사용자 리뷰 수정 API")
+    class updateReview {
+        @Test
+        @DisplayName("작성한 리뷰가 없으면 수정할 수 없다")
+        void failure() {
+            작성한_리뷰를_수정한다(hostAccessToken, memberId)
+                    .statusCode(NOT_FOUND.value());
+        }
+
+        @Test
+        @DisplayName("작성한 리뷰를 수정한다")
+        void success() {
+            스터디_신청자에_대한_참여를_승인한다(hostAccessToken, studyId, memberId);
+            스터디_주차를_생성한다(hostAccessToken, studyId, STUDY_WEEKLY_1);
+            해당_사용자에게_리뷰를_작성한다(hostAccessToken, memberId);
+
+            작성한_리뷰를_수정한다(hostAccessToken, memberId)
+                    .statusCode(NO_CONTENT.value());
+        }
+    }
 }
