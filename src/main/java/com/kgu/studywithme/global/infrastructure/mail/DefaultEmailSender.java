cb1e0b4ccbdd84a26d2db6ab2b57c58b2beba9ff
SeoJiWon <sjiwon4491@gmail.com>
2023-08-10 14:02:36 +0900

    refactor: 이메일 템플릿 내부 사진 S3 업로드 후 링크 적용, DefaultEmailSender `@Profile` 적용

--- src/main/java/com/kgu/studywithme/global/infrastructure/mail/DefaultEmailSender.java
+++ src/main/java/com/kgu/studywithme/global/infrastructure/mail/DefaultEmailSender.java
@@ -4,7 +4,7 @@ import com.kgu.studywithme.global.infrastructure.mail.utils.EmailMetadata;
 import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.context.annotation.Profile;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
@@ -12,7 +12,9 @@ import org.thymeleaf.context.Context;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 
 import java.time.LocalDate;
+import java.util.Map;
 
+@Profile("default")
 @Component
 public class DefaultEmailSender implements EmailSender {
     private final JavaMailSender mailSender;
@@ -29,77 +31,87 @@ public class DefaultEmailSender implements EmailSender {
         this.serviceEmail = serviceEmail;
     }
 
+    @Override
     public void sendParticipationApproveMail(
             final String applierEmail,
             final String nickname,
             final String studyName
     ) throws Exception {
-        final Context context = new Context();
-        context.setVariable("nickname", nickname);
-        context.setVariable("studyName", studyName);
+        final Map<String, Object> variables = Map.of(
+                "nickname", nickname,
+                "studyName", studyName
+        );
+        final Context context = createTemplateContext(variables);
 
         final String mailBody = templateEngine.process(EmailMetadata.APPROVE_TEMPLATE, context);
         sendMail(
                 EmailMetadata.PARTICIPATION_SUBJECT,
                 applierEmail,
-                mailBody,
-                new ClassPathResource("static/images/top.png")
+                mailBody
         );
     }
 
+    @Override
     public void sendParticipationRejectMail(
             final String applierEmail,
             final String nickname,
             final String studyName,
             final String reason
     ) throws Exception {
-        final Context context = new Context();
-        context.setVariable("nickname", nickname);
-        context.setVariable("studyName", studyName);
-        context.setVariable("reason", reason);
+        final Map<String, Object> variables = Map.of(
+                "nickname", nickname,
+                "studyName", studyName,
+                "reason", reason
+        );
+        final Context context = createTemplateContext(variables);
 
         final String mailBody = templateEngine.process(EmailMetadata.REJECT_TEMPLATE, context);
         sendMail(
                 EmailMetadata.PARTICIPATION_SUBJECT,
                 applierEmail,
-                mailBody,
-                new ClassPathResource("static/images/top.png")
+                mailBody
         );
     }
 
+    @Override
     public void sendStudyCertificateMail(
             final String participantEmail,
             final String nickname,
             final String studyName
     ) throws Exception {
-        final Context context = new Context();
-        context.setVariable("nickname", nickname);
-        context.setVariable("studyName", studyName);
-        context.setVariable("completionDate", LocalDate.now().format(EmailMetadata.DATE_TIME_FORMATTER));
+        final Map<String, Object> variables = Map.of(
+                "nickname", nickname,
+                "studyName", studyName,
+                "completionDate", LocalDate.now().format(EmailMetadata.DATE_TIME_FORMATTER)
+        );
+        final Context context = createTemplateContext(variables);
 
         final String mailBody = templateEngine.process(EmailMetadata.CERTIFICATE_TEMPLATE, context);
         sendMail(
                 EmailMetadata.COMPLETION_SUBJECT,
                 participantEmail,
-                mailBody,
-                new ClassPathResource("static/images/stamp.png")
+                mailBody
         );
     }
 
+    private Context createTemplateContext(final Map<String, Object> variables) {
+        final Context context = new Context();
+        context.setVariables(variables);
+        return context;
+    }
+
     private void sendMail(
-            final String subjectType,
+            final String subject,
             final String email,
-            final String mailBody,
-            final ClassPathResource imageResource
+            final String mailBody
     ) throws Exception {
         final MimeMessage message = mailSender.createMimeMessage();
         final MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
 
-        helper.setSubject(subjectType);
+        helper.setSubject(subject);
         helper.setTo(email);
         helper.setFrom(new InternetAddress(serviceEmail, "여기서 구해볼래?"));
         helper.setText(mailBody, true);
-        helper.addInline("image", imageResource);
 
         mailSender.send(message);
     }
