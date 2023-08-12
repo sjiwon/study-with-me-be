9b9cfd3f8153cf6ce13c875ef77ce53d710e169b
SeoJiWon <sjiwon4491@gmail.com>
2023-08-12 14:58:04 +0900

    refactor: Google UserInfo 응답 스펙 GoogleUserResponse에 전부 포함

--- src/main/java/com/kgu/studywithme/auth/infrastructure/oauth/google/response/GoogleUserResponse.java
+++ src/main/java/com/kgu/studywithme/auth/infrastructure/oauth/google/response/GoogleUserResponse.java
@@ -4,14 +4,35 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.kgu.studywithme.auth.infrastructure.oauth.OAuthUserResponse;
 
 public record GoogleUserResponse(
+        // required
         String name,
-        String email
+        String email,
+
+        // other
+        String sub,
+        String givenName,
+        String familyName,
+        String picture,
+        boolean emailVerified,
+        String locale
 ) implements OAuthUserResponse {
     public GoogleUserResponse(
             @JsonProperty("name") final String name,
-            @JsonProperty("email") final String email
+            @JsonProperty("email") final String email,
+            @JsonProperty("sub") final String sub,
+            @JsonProperty("given_name") final String givenName,
+            @JsonProperty("family_name") final String familyName,
+            @JsonProperty("picture") final String picture,
+            @JsonProperty("email_verified") final boolean emailVerified,
+            @JsonProperty("locale") final String locale
     ) {
         this.name = name;
         this.email = email;
+        this.sub = sub;
+        this.givenName = givenName;
+        this.familyName = familyName;
+        this.picture = picture;
+        this.emailVerified = emailVerified;
+        this.locale = locale;
     }
 }
--- src/main/java/com/kgu/studywithme/member/exception/MemberErrorCode.java
+++ src/main/java/com/kgu/studywithme/member/exception/MemberErrorCode.java
@@ -12,7 +12,7 @@ import static org.springframework.http.HttpStatus.*;
 public enum MemberErrorCode implements ErrorCode {
     INVALID_NICKNAME_FORMAT(BAD_REQUEST, "MEMBER_001", "닉네임 형식에 맞지 않습니다."),
     NICKNAME_SAME_AS_BEFORE(CONFLICT, "MEMBER_002", "이전과 동일한 닉네임으로 수정할 수 없습니다."),
-    INVALID_EMAIL_FORMAT(BAD_REQUEST, "MEMBER_003", "구글 이메일 형식에 맞지 않습니다."),
+    INVALID_EMAIL_FORMAT(BAD_REQUEST, "MEMBER_003", "구글, 네이버, 카카오 이메일만 허용합니다."),
     INVALID_GENDER(BAD_REQUEST, "MEMBER_004", "유효하지 않은 성별입니다."),
     REGION_IS_BLANK(BAD_REQUEST, "MEMBER_005", "거주지를 정확하게 입력해주세요."),
     INTEREST_MUST_EXISTS_AT_LEAST_ONE(BAD_REQUEST, "MEMBER_006", "관심사는 적어도 1개 이상 등록해야 합니다."),
