7d9fa9b0827b2d4a060dc12a844ed0e36b6ebd30
SeoJiWon <sjiwon4491@gmail.com>
2023-08-24 22:23:54 +0900

    refactor: Hashtag (study_id, name) Index 추가

--- src/main/resources/db/migration/V8__add_index_for_study_participation_query_optimization.sql
+++ src/main/resources/db/migration/V8__add_index_for_study_participation_query_optimization.sql
@@ -1,3 +1,7 @@
 -- 신청자 Validation 관련 쿼리 최적화
 ALTER TABLE study_participant
-    ADD INDEX idx_participant_member_id_status_study_id (member_id, status, study_id);
+    ADD INDEX idx_participant_study_id_member_id_status (study_id, member_id, status);
+
+-- 스터디 해시태그 조회 쿼리 최적화
+ALTER TABLE study_hashtag
+    ADD INDEX idx_hashtag_study_id_name (study_id, name);
