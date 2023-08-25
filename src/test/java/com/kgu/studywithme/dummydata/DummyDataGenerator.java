9a9599840b0a67e22c7c93d4bdcc2b7022b65d82
SeoJiWon <sjiwon4491@gmail.com>
2023-08-25 15:25:34 +0900

    style: optimize import

--- src/test/java/com/kgu/studywithme/dummydata/DummyDataGenerator.java
+++ src/test/java/com/kgu/studywithme/dummydata/DummyDataGenerator.java
@@ -7,7 +7,6 @@ import com.kgu.studywithme.file.application.adapter.FileUploader;
 import com.kgu.studywithme.mail.application.adapter.EmailSender;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
