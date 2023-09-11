3f22f2d6098eaa4feff3ed6220aa27c5bcee24c6
SeoJiWon <sjiwon4491@gmail.com>
2023-09-11 19:51:12 +0900

    refactor: Email Async 관련 `ThreadPoolTaskExecutor` 설정 수정

--- src/main/java/com/kgu/studywithme/global/config/AsyncConfiguration.java
+++ src/main/java/com/kgu/studywithme/global/config/AsyncConfiguration.java
@@ -6,13 +6,14 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
 
 @Configuration
 @EnableAsync
 public class AsyncConfiguration {
-    private static final int CORE_POOL_SIZE = 10;
-    private static final int MAX_POOL_SIZE = 25;
-    private static final int QUEUE_CAPACITY = 10;
+    private static final int CORE_POOL_SIZE = 5;
+    private static final int MAX_POOL_SIZE = 20;
+    private static final int QUEUE_CAPACITY = 50;
     private static final String EMAIL_ASYNC_THREAD_NAME_PREFIX = "Asynchronous Mail Sender Thread-";
 
     @Bean(name = "emailAsyncExecutor")
@@ -21,6 +22,8 @@ public class AsyncConfiguration {
         executor.setCorePoolSize(CORE_POOL_SIZE);
         executor.setMaxPoolSize(MAX_POOL_SIZE);
         executor.setQueueCapacity(QUEUE_CAPACITY);
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        executor.setAwaitTerminationSeconds(60);
         executor.setThreadNamePrefix(EMAIL_ASYNC_THREAD_NAME_PREFIX);
         executor.initialize();
         return executor;
