395badf7d40a02ac1762c93b81848371e6c725e5
SeoJiWon <sjiwon4491@gmail.com>
2023-10-29 11:59:59 +0900

    chore: 새로 띄운 Docker Container 실행 관련 옵션 -aq -> -q
    - 실행중인 Container만 확인하기 위해서 -a 옵션 제거

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,13 +10,13 @@ cd $DEPLOY_PATH
 echo "> 0. 배포 위치 = $DEPLOY_PATH" >> $DEPLOY_LOG_PATH
 
 CURRENT_RUNNING_PID=$(sudo docker container ls -aq -f name=application)
-echo "> 1. 현재 실행 중인 Docker Container PID = $CURRENT_RUNNING_PID" >> $DEPLOY_LOG_PATH
+echo "> 1. 현재 관리중인 WAS Docker Container PID = $CURRENT_RUNNING_PID" >> $DEPLOY_LOG_PATH
 
 if [ -z $CURRENT_RUNNING_PID ]
 then
-  echo "> 2-1. 현재 구동중인 Docker Container가 없습니다" >> $DEPLOY_LOG_PATH
+  echo "> 2-1. 현재 관리중인 WAS Docker Container가 없습니다" >> $DEPLOY_LOG_PATH
 else
-  echo "> 2-2-1. 현재 구동중인 Docker Container Stop & Remove = $CURRENT_RUNNING_PID" >> $DEPLOY_LOG_PATH
+  echo "> 2-2-1. 현재 관리중인 WAS Docker Container Stop & Remove = $CURRENT_RUNNING_PID" >> $DEPLOY_LOG_PATH
   echo "> 2-2-2. sudo docker stop $CURRENT_RUNNING_PID" >> $DEPLOY_LOG_PATH
   sudo docker stop $CURRENT_RUNNING_PID
   echo "> 2-2-3. sudo docker rm $CURRENT_RUNNING_PID" >> $DEPLOY_LOG_PATH
@@ -39,7 +39,7 @@ sudo docker run \
       -v $PINPOINT_PATH:/app/pinpoint \
       $DOCKER_IMAGE:$DOCKER_IMAGE_TAG
 
-NEW_RUNNING_PID=$(sudo docker container ls -aq -f name=application)
-echo "> 4. 새로 실행된 Docker Container PID = $NEW_RUNNING_PID" >> $DEPLOY_LOG_PATH
+NEW_RUNNING_PID=$(sudo docker container ls -q -f name=application)
+echo "> 4. 새로 실행된 WAS Docker Container PID = $NEW_RUNNING_PID" >> $DEPLOY_LOG_PATH
 
 echo -e "> ##### END #####\n" >> $DEPLOY_LOG_PATH
