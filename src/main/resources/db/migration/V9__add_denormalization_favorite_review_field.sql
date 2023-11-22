20ea17f6df0518a46e0c5e9e9968b7c4f6193f4b
SeoJiWon <sjiwon4491@gmail.com>
2023-11-22 17:33:44 +0900

    chore: 반정규화 필드 적용 Flyway 스키마 추가

---
+++ src/main/resources/db/migration/V9__add_denormalization_favorite_review_field.sql
@@ -0,0 +1,8 @@
+ALTER TABLE study
+    ADD favorite_count INT NOT NULL DEFAULT 0;
+
+ALTER TABLE study
+    ADD review_count INT NOT NULL DEFAULT 0;
+
+ALTER TABLE study
+    ADD version BIGINT NOT NULL DEFAULT 0;
