e0c3d98e2bb44ad13bcf22c4521981c2ef1d63a4
SeoJiWon <sjiwon4491@gmail.com>
2023-12-14 20:08:13 +0900

    test: RedisTestContainers 적용

--- src/test/java/com/kgu/studywithme/common/IntegrateTest.java
+++ src/test/java/com/kgu/studywithme/common/IntegrateTest.java
@@ -2,6 +2,7 @@ package com.kgu.studywithme.common;
 
 import com.kgu.studywithme.common.config.DatabaseCleanerEachCallbackExtension;
 import com.kgu.studywithme.common.config.MySqlTestContainersExtension;
+import com.kgu.studywithme.common.config.RedisTestContainersExtension;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -11,7 +12,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 @ExtendWith({
         DatabaseCleanerEachCallbackExtension.class,
-        MySqlTestContainersExtension.class
+        MySqlTestContainersExtension.class,
+        RedisTestContainersExtension.class
 })
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public abstract class IntegrateTest {
