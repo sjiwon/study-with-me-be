88d2da50efd190010205d46ea7b5db139442f1bc
sjiwon <sjiwon4491@gmail.com>
2023-12-15 15:11:19 +0900

    [fix] Scale Out 후 스케줄링 로직이 중복 실행되는 문제를 해결한다 (#29)
    * feat: 스케줄링 중복 실행 Redis setnx를 통해서 제어

--- src/main/java/com/kgu/studywithme/global/scheduler/UpdateWeeklyAttendanceScheduler.java
+++ src/main/java/com/kgu/studywithme/global/scheduler/UpdateWeeklyAttendanceScheduler.java
@@ -2,16 +2,26 @@ package com.kgu.studywithme.global.scheduler;
 
 import com.kgu.studywithme.studyattendance.domain.service.UpdateWeeklyAttendanceBatchProcessor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.time.Duration;
 
 @Component
 @RequiredArgsConstructor
 public class UpdateWeeklyAttendanceScheduler {
+    private final StringRedisTemplate redisTemplate;
     private final UpdateWeeklyAttendanceBatchProcessor updateWeeklyAttendanceBatchProcessor;
 
     @Scheduled(cron = "0 0 0 * * *", zone = "Asia/Seoul")
     public void processAbsenceCheckScheduler() {
-        updateWeeklyAttendanceBatchProcessor.checkAbsenceParticipantAndApplyAbsenceScore();
+        if (canExecute()) {
+            updateWeeklyAttendanceBatchProcessor.checkAbsenceParticipantAndApplyAbsenceScore();
+        }
+    }
+
+    private boolean canExecute() {
+        return Boolean.TRUE.equals(redisTemplate.opsForValue().setIfAbsent("scheduling", "on", Duration.ofMinutes(5)));
     }
 }
