26cc7d546810bc674b8449f7dd32402cc7c3e9fb
SeoJiWon <sjiwon4491@gmail.com>
2023-12-18 16:40:57 +0900

    feat: 첨부파일 업로드 CompletableFuture + 커스텀 ThreadPool 적용

--- src/main/java/com/kgu/studywithme/global/config/AsyncConfiguration.java
+++ src/main/java/com/kgu/studywithme/global/config/AsyncConfiguration.java
@@ -16,7 +16,7 @@ import java.util.concurrent.ThreadPoolExecutor;
 @EnableAsync
 public class AsyncConfiguration implements AsyncConfigurer {
     @Bean(name = "emailAsyncExecutor")
-    public Executor getAsyncExecutor() {
+    public Executor emailAsyncExecutor() {
         final ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(10);
         executor.setMaxPoolSize(40);
@@ -24,6 +24,18 @@ public class AsyncConfiguration implements AsyncConfigurer {
         executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
         executor.setAwaitTerminationSeconds(60);
         executor.setThreadNamePrefix("Asynchronous Mail Sender Thread-");
+        return executor;
+    }
+
+    @Bean(name = "fileUploadExecutor")
+    public Executor fileUploadExecutor() {
+        final ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(40);
+        executor.setQueueCapacity(50);
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        executor.setAwaitTerminationSeconds(60);
+        executor.setThreadNamePrefix("Asynchronous File Upload Thread-");
         return executor;
     }
 
--- src/main/java/com/kgu/studywithme/studyweekly/domain/service/AttachmentUploader.java
+++ src/main/java/com/kgu/studywithme/studyweekly/domain/service/AttachmentUploader.java
@@ -3,24 +3,41 @@ package com.kgu.studywithme.studyweekly.domain.service;
 import com.kgu.studywithme.file.application.adapter.FileUploader;
 import com.kgu.studywithme.file.domain.model.RawFileData;
 import com.kgu.studywithme.studyweekly.domain.model.UploadAttachment;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
 
 @Component
-@RequiredArgsConstructor
 public class AttachmentUploader {
     private final FileUploader fileUploader;
+    private final Executor executor;
+
+    public AttachmentUploader(
+            final FileUploader fileUploader,
+            @Qualifier("fileUploadExecutor") final Executor executor
+    ) {
+        this.fileUploader = fileUploader;
+        this.executor = executor;
+    }
 
     public List<UploadAttachment> uploadAttachments(final List<RawFileData> files) {
         if (CollectionUtils.isEmpty(files)) {
             return List.of();
         }
 
-        return files.stream()
-                .map(file -> new UploadAttachment(file.fileName(), fileUploader.uploadFile(file)))
+        final List<CompletableFuture<UploadAttachment>> result = files.stream()
+                .map(file -> CompletableFuture.supplyAsync(
+                        () -> new UploadAttachment(file.fileName(), fileUploader.uploadFile(file)),
+                        executor
+                ))
+                .toList();
+
+        return result.stream()
+                .map(CompletableFuture::join)
                 .toList();
     }
 }
