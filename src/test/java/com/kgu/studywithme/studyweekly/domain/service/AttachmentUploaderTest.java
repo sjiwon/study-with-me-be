43dd9788345d2ac07bd08472ac13b68fdf4fb26c
SeoJiWon <sjiwon4491@gmail.com>
2023-12-18 16:41:18 +0900

    test: 첨부파일 단위 테스트 관련 CompletableFuture로 인한 Asynchronous 무한 대기 -> `@SpringBootTest`로 수정

--- src/test/java/com/kgu/studywithme/studyweekly/domain/service/AttachmentUploaderTest.java
+++ src/test/java/com/kgu/studywithme/studyweekly/domain/service/AttachmentUploaderTest.java
@@ -1,6 +1,5 @@
 package com.kgu.studywithme.studyweekly.domain.service;
 
-import com.kgu.studywithme.common.ParallelTest;
 import com.kgu.studywithme.common.mock.stub.StubFileUploader;
 import com.kgu.studywithme.file.application.adapter.FileUploader;
 import com.kgu.studywithme.file.domain.model.RawFileData;
@@ -9,18 +8,44 @@ import com.kgu.studywithme.studyweekly.domain.model.UploadAttachment;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 import static com.kgu.studywithme.common.utils.FileMockingUtils.createMultipleMockMultipartFile;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
+/**
+ * CompletableFuture asynchronous로 인해 단위 테스트에서 무한 대기하는 현상 발생 <br>
+ * -> 임시 대안으로 @SpringBootTest + AttachmentUploader만 Context에 띄워서 진행
+ */
+@SpringBootTest(classes = {AttachmentUploader.class})
+@Import(AttachmentUploaderTest.AttachmentUploaderTestConfiguration.class)
 @DisplayName("StudyWeekly -> AttachmentUploader 테스트")
-public class AttachmentUploaderTest extends ParallelTest {
-    private final FileUploader fileUploader = new StubFileUploader();
-    private final AttachmentUploader sut = new AttachmentUploader(fileUploader);
+public class AttachmentUploaderTest {
+    @TestConfiguration
+    static class AttachmentUploaderTestConfiguration {
+        @Bean
+        public FileUploader fileUploader() {
+            return new StubFileUploader();
+        }
+
+        @Bean
+        public Executor fileUploadExecutor() {
+            return Executors.newFixedThreadPool(10);
+        }
+    }
+
+    @Autowired
+    private AttachmentUploader sut;
 
     private List<RawFileData> files;
 
